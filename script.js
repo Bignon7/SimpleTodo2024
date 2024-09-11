df14356f610eda4fcbf7cc807864d5c7caa7792c
Bignon7 <hindemelais@gmail.com>
2024-09-11 19:01:11 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let addInput = document.getElementById("add_input");
+let listContainer = document.getElementById("list_container");
+
+function addTask() {
+  if (addInput.value === "") {
+    alert("Une tâche ne peut pas être vide voyons");
+  } else {
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    let hr = document.createElement("hr");
+    p.classList.add("row");
+    let textSpan = document.createElement("span");
+    textSpan.innerHTML = addInput.value;
+    textSpan.classList.add("textLine");
+    let iconSpan = document.createElement("span");
+    iconSpan.innerHTML = `<i class="fa-regular fa-trash-can"></i>`;
+    iconSpan.classList.add("icon");
+
+    p.appendChild(textSpan);
+    p.appendChild(iconSpan);
+    li.appendChild(p);
+    li.appendChild(hr);
+    listContainer.appendChild(li);
+  }
+  addInput.value = "";
+  saveTask();
+}
+
+listContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "SPAN" || e.target.classList.contains("textLine")) {
+    e.target.parentNode.classList.toggle("checked");
+    saveTask();
+  } else if (
+    e.target.parentElement.tagName === "SPAN" ||
+    e.target.parentElement.classList.contains("icon")
+  ) {
+    const listItem = e.target.closest("li");
+    if (listItem) {
+      listItem.remove();
+      saveTask();
+    }
+  }
+});
+
+function saveTask() {
+  localStorage.setItem("tasksContainer", listContainer.innerHTML);
+}
+
+function setTask() {
+  listContainer.innerHTML = localStorage.getItem("tasksContainer");
+}
+setTask();
